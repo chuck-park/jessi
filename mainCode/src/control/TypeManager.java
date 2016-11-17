6fb7486dc5fc66633833647daae6fc9bd06a63a1
Chungmin Park <chuck.m.park@gmail.com>
2016-11-17 23:20:07 +0900

    make search method, vector data structure

--- mainCode/src/control/TypeManager.java
+++ mainCode/src/control/TypeManager.java
@@ -14,6 +14,5 @@ public class TypeManager {
 		Vector<TypeInfo> typelist = type.getList();
 		return typelist;
 	}
-
 }
 
--- mainCode/src/entity/Recipe.java
+++ mainCode/src/entity/Recipe.java
@@ -6,31 +6,63 @@ import java.util.*;
 import valueObject.*;
 
 public class Recipe {
-  private Vector<Recipeinfo> recipelist1;
+  private Vector<RecipeInfo> recipelist;
   private Vector<String> ingredientlist1;
   private Vector<String> ingredientlist2;
   private Vector<String> ingredientlist3;
   private Vector<String> ingredientlist4;
   
-  public Recipe(){
-    recipelist1.add("햄");
+  public Recipe(){ // 생성시 레시피 별 재료리스트 추가
+    this.recipelist = new Vector<RecipeInfo>();
+    this.ingredientlist1 = new Vector<String>();
+    this.ingredientlist2 = new Vector<String>();
+    this.ingredientlist3 = new Vector<String>();
+    this.ingredientlist4 = new Vector<String>();
+    
+    ingredientlist1.add("햄");
+    ingredientlist1.add("돼지고기");
+    ingredientlist1.add("소시지");
+    ingredientlist1.add("양파");
+    ingredientlist1.add("고추장");
+    
+    ingredientlist2.add("닭고기");
+    ingredientlist2.add("간장");
+    ingredientlist2.add("식초");
+    ingredientlist2.add("물엿");
+    
+    ingredientlist3.add("낙지");
+    ingredientlist3.add("양배추");
+    ingredientlist3.add("양파");
+    ingredientlist3.add("당근");
+    
+    ingredientlist4.add("배추");
+    ingredientlist4.add("고추가루");
+    ingredientlist4.add("소금");
+    
+    recipelist.add(new RecipeInfo("부대찌개", ingredientlist1));
+    recipelist.add(new RecipeInfo("연근조림", ingredientlist2));
+    recipelist.add(new RecipeInfo("낚지볶음", ingredientlist3));
+    recipelist.add(new RecipeInfo("배추김치", ingredientlist4));
+    
   }
-  public Vector<Recipeinfo> getList() {
-    try {
-
-      BufferedReader fileReader = new BufferedReader(new FileReader("type.txt"));
-      recipelist = new Vector<Recipeinfo>();
-
-      recipelist.add(new Recipeinfo("부대찌개");
-      recipelist.add(new TypeInfo(fileReader.read(), fileReader.readLine()));
-      recipelist.add(new TypeInfo(fileReader.read(), fileReader.readLine()));
-      recipelist.add(new TypeInfo(fileReader.read(), fileReader.readLine()));
-
-      fileReader.close();
-    } catch (IOException e) {
-      // TODO Auto-generated catch block
-      e.printStackTrace();
+  
+  // selectlist와 recipelist의 재료 일치 비교 검색
+  public void search(Vector<IngredientInfo> selectlist){
+    int k = 0;
+    for(int i = 0; i < selectlist.size(); i++){
+      for(int j = 0; j < recipelist.size(); j++){
+        for(k = 0; k < recipelist.get(j).getIngredient().size(); k++){
+          //System.out.println(recipelist.get(j).getIngredient().size());
+          System.out.println(selectlist.get(i).getIngredientName() + " ==? " + recipelist.get(j).getIngredient().get(k));
+          if(selectlist.get(i).getIngredientName().equals(recipelist.get(j).getIngredient().get(k))){
+            System.out.println("ok");
+            recipelist.get(j).countUp(); // 일치하면 해당 recipe의 count++
+          }
+        }
+        k = 0;
+      }
     }
-    return recipelist;
+    for(int a = 0; a < recipelist.size(); a++)
+    System.out.println(a + "번째 레시피의 count : " + recipelist.get(a).getCount());
   }
 }
