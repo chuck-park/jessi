967168065204c81f76f2a652ad7f9b72dbbf86bf
Chungmin Park <chuck.m.park@gmail.com>
2016-11-26 01:10:18 +0900

    add like sorting with bubble sort

--- mainCode/src/entity/Recipe.java
+++ mainCode/src/entity/Recipe.java
@@ -1,12 +1,14 @@
 package entity;
 
 import java.io.*;
+
 import java.util.*;
 
 import valueObject.*;
 
 public class Recipe {
   private Vector<RecipeInfo> recipelist;
+  private Vector<RecipeInfo> likelist;
   private Vector<String> ingredientlist1;
   private Vector<String> ingredientlist2;
   private Vector<String> ingredientlist3;
@@ -65,4 +67,89 @@ public class Recipe {
     for(int a = 0; a < recipelist.size(); a++)
     System.out.println(a + "번째 레시피의 count : " + recipelist.get(a).getCount());
   }
-}
+  
+  public int getLikelist(){
+	  recipelist.get(0).setLike(4);
+	  recipelist.get(1).setLike(5);
+	  recipelist.get(2).setLike(1);
+	  
+	  System.out.println("recipelist size print : " + recipelist.size());
+	  
+	  this.likelist = new Vector <RecipeInfo>(); // 좋아요가 1이상인 list에 레시피인포 생성
+	  
+	  for(int x=0; x<recipelist.size();x++){ 
+		  if(recipelist.get(x).getLike()!=0){ // list에 좋아요가 0이 아닌 리스트를 넣는다
+			  likelist.add(recipelist.get(x));
+			  System.out.println("likelist "+x + " print : " + likelist.size());
+		  }
+	  }
+	  return likelist.size();
+  }
+  
+  public void sortLike(){
+	  System.out.println("print : " + likelist.get(0).getLike());
+	  //quickSort(likelist, 0, likelist.size() );
+	  bubbleSort(likelist);
+	  System.out.println("print : " + likelist.get(0).getLike());
+	  
+  }
+  
+  void bubbleSort(Vector<RecipeInfo> likelist) {
+
+	    int n = likelist.size();
+	    Vector<RecipeInfo> temp = new Vector<RecipeInfo>();
+
+	    for (int i = 0; i < n; i++) {
+	        for (int j = 1; j < (n - i); j++) {
+
+	            if (likelist.get(j-1).getLike() > likelist.get(j).getLike()) {
+	            	temp.add(0, likelist.get(j-1));
+	            	  likelist.add(j-1, likelist.get(j));
+	            	  likelist.add(j, temp.get(0));
+	            }
+
+	        }
+	    }
+	}
+  
+  
+  
+  /*
+  int partition(Vector<RecipeInfo> likelist, int left, int right)
+  {
+        int i = left, j = right;
+        int middle = left + (right - left) /2;
+        Vector<RecipeInfo> temp = new Vector<RecipeInfo>();
+        int pivot = likelist.get(middle).getLike();
+       
+        while (i <= j) {
+              while (likelist.get(left).getLike() < pivot)
+                    i++;
+              while (likelist.get(right).getLike() > pivot)
+                    j--;
+              if (i <= j) {
+            	  temp.add(0, likelist.get(left));
+            	  likelist.add(left, likelist.get(right));
+            	  likelist.add(right, temp.get(0));
+                    i++;
+                    j--;
+              }
+        };
+       
+        return i;
+  }
+   
+  void quickSort(Vector<RecipeInfo> likelist, int left, int right) {
+        int index = partition(likelist, left, right);
+        if (left < index - 1)
+              quickSort(likelist, left, index - 1);
+        if (index < right)
+              quickSort(likelist, index, right);
+  }
+  
+  */
+  
+ 
+ }
+
+
